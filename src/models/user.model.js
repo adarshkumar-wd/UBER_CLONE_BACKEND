ec3262d3c966ded69bb4ea5269e950f65e8c837d
Adarsh Kumar <adarshff06@gmail.com>
2024-12-22 23:57:39 +0530

    complete user Model

---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true,
+    },
+
+    lastName : {
+        type : String,
+    },
+
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true
+    },
+
+    password : {
+        type : String,
+        required : true,
+        trim : true
+    },
+
+    socketId : {}
+} , {});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const userModel = mongoose.model("user" , userSchema)
